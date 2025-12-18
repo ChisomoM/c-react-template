9a9533aca6a33f8bc8c352fa18f60355b7a3725f
ChisomoM <kachizo95@gmail.com>
2025-12-18 22:22:22 +0200

    Updates

--- src/(public)/home/home.tsx
+++ src/(public)/home/home.tsx
@@ -2,6 +2,7 @@
 import Hero from '@/(public)/home/components/hero';
 import Navbar from '../../components/navbar';
 import { Footer } from '@/components/footer';
+import SEO from '../../components/SEO';
 // import { ScrollytellingFeatures } from './components/ScrollytellingFeatures';
 
 
@@ -9,6 +10,12 @@ import { Footer } from '@/components/footer';
 export default function Home(){
     return(
         <div className="pt-20">
+            <SEO
+                title="Home - Your Site Name"
+                description="Welcome to our homepage. Discover our services and features."
+                keywords="home, welcome, services, features"
+                type="website"
+            />
             <Navbar/>
             <Hero/>
 
